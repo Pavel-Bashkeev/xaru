8725207e9d27d43910d726bea05a1d8979c2ee73
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-09-09 12:30:23 +0300

    bag svg

--- app/js/vendor/myParticles.js
+++ app/js/vendor/myParticles.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles-js', '../data/particlesjs-config.json', function() {
+particlesJS.load('particles-js', 'https://pavel-bashkeev.github.io/xaru/dist/particlesjs-config.json', function() {
   console.log('particles.js loaded - callback');
 });
