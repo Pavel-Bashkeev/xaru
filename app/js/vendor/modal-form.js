205fb577e253517f21509fddcdcda0bb8a8550ea
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-09-09 15:00:20 +0300

    optim

--- app/js/vendor/modal-form.js
+++ app/js/vendor/modal-form.js
@@ -1,27 +1,32 @@
-const btnLink = document.querySelectorAll('.btn-link');
-const modalForm = document.querySelector('.modal-form');
-const closeBtn = document.querySelector('.close-btn');
-closeBtn.addEventListener('click', ()=>{
-  modalForm.classList.add('hide');
-    document.body.classList.remove('hidden');
-})
-btnLink.forEach(item => {
-  item.addEventListener('click' , showModalForm);
+window.addEventListener('DOMContentLoaded', () => {
+  const btnLink = document.querySelectorAll('.btn-link');
+  const modalForm = document.querySelector('.modal-form');
+  const closeBtn = document.querySelector('.close-btn');
+  if (btnLink) {
+    closeBtn.addEventListener('click', () => {
+      modalForm.classList.add('hide');
+      document.body.classList.remove('hidden');
+    })
+    btnLink.forEach(item => {
+      item.addEventListener('click', showModalForm);
+
+    });
+
+    function showModalForm() {
+      modalForm.classList.toggle('hide');
+      document.body.classList.add('hidden')
+    }
+
+    modalForm.addEventListener('click', function (e) {
+      let target = e.target;
+      let form = document.querySelector('.form');
+      let itsForm = target.classList.contains('form');
+      let itsFormElem = form.contains(target);
+      if (!itsForm && !itsFormElem) {
+        modalForm.classList.add('hide');
+        document.body.classList.remove('hidden')
+      }
+    })
+  }
 
 });
-
-function showModalForm() {
-  modalForm.classList.toggle('hide');
-  document.body.classList.add('hidden')
-}
-
-modalForm.addEventListener('click', function(e){
-  let target = e.target;
-  let form = document.querySelector('.form');
-  let itsForm = target.classList.contains('form');
-  let itsFormElem = form.contains(target);
-  if(!itsForm && !itsFormElem){
-    modalForm.classList.add('hide');
-    document.body.classList.remove('hidden')
-  }
-})
--- app/js/vendor/myParticles.js
+++ app/js/vendor/myParticles.js
@@ -1,4 +1,4 @@
-
+window.addEventListener('DOMContentLoaded', () => {
 particlesJS('particles-js',
   
 {
@@ -113,3 +113,6 @@ particlesJS('particles-js',
 }
 
 );
+
+})
+
