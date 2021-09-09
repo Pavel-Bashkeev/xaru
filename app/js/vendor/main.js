9944bdd8591c65f36ea41854d7bf66edbb271054
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-09-09 03:32:44 +0300

    end desktop

--- app/js/vendor/main.js
+++
@@ -1,4 +0,0 @@
-
-import { num } from '../components/test.js'
-
-console.log(num);
---
+++ app/js/vendor/parallax.js
@@ -0,0 +1,24 @@
+// const cardTop = document.querySelector('.card-top');
+// const cargBottom = document.querySelector('.card-bottom');
+// const dollarFour = document.querySelector('.decor-dol-4');
+// const dollarSecond = document.querySelector('.decor-dol-2');
+// const cardDecor =document.querySelector('.card-content__decor');
+
+// console.log(cardTop.offsetTop)
+//  cardDecor.addEventListener('mousemove', function(event){
+//    moveElement(event, cardTop);
+//    moveElement(event, cargBottom);
+//   });
+
+//  function moveElement(event , elem) {
+//   let offsetX = Math.floor((((event.clientX / cardDecor.clientWidth * 5) - 5) + elem.offsetLeft) - elem.offsetLeft);
+//   let offsetY = Math.floor((((event.clientY / cardDecor.clientHeight * 5) - 5) + elem.offsetTop) - elem.offsetTop);
+//   elem.style.top = offsetY + 'px';
+//   elem.style.left = offsetX + 'px';
+//   // console.log(elem.style.top );
+//  }
+
+//  console.dir(cardTop);
+//  console.dir(cargBottom);
+//  console.dir(dollarFour);
+//  console.dir(dollarSecond);
