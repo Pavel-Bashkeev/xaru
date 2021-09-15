29025afbdefe3a81a001bea5c7eb9b72a23124e6
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-09-15 12:52:18 +0300

    move i

--- app/js/vendor/animCard.js
+++ app/js/vendor/animCard.js
@@ -1,7 +1,7 @@
-document.addEventListener('mousemove', moveElemCard);
-document.addEventListener('scroll', moveElemCard)
-function moveElemCard(event){
-  document.querySelectorAll('.card-content__decor-img').forEach(function(move){
+document.addEventListener('mousemove', moveElemCardMouse);
+
+function moveElemCardMouse(event){
+  document.querySelectorAll('.card-content__decor-i').forEach(function(move){
 
     let moving_value = move.getAttribute("data-value");
     let x = (event.clientX * moving_value) / 250;
