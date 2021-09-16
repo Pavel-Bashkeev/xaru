9bdbe92127bb77ae6240eb694391e39e8d1252db
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-09-16 09:26:50 +0300

    super particles

--- app/js/vendor/myParticles.js
+++ app/js/vendor/myParticles.js
@@ -1,24 +1,30 @@
-window.onload = function () {
-  var divContainer = '#super-particles'
-  new SuperParticles({
-    container: divContainer,
-    maxFps: 30,
-    autoStartAnimation: true,
-    particles: {
-    amount: 120, 
-    radius: 3,
-    velocity : 20,
-    color: "0x1f8efa", 
-    fadeInDuration: 3000, 
-    fadeOutDuration: 600, 
-    keepRelativePositionOnResize: true,
-},
-lines: {
-    minDistance: 0.15,
-    color: "0xFFFFFF",
-    maxOpacity: 0.4,
-    thickness: 1,
-    distanceBasedTransparency: true,
-},
-  })
+if(document.body.clientWidth > 420){
+  window.onload = function () {
+    var divContainer = '#super-particles'
+    new SuperParticles({
+      container: {
+        element : divContainer,
+        backgroundCssRule: "transparent"
+      },
+      maxFps: 30,
+      autoStartAnimation: true,
+      particles: {
+      amount: 220, 
+      radius: 3,
+      velocity : 20,
+      color: "0x1f8efa",
+      maxOpacity: 0.4, 
+      fadeInDuration: 1000, 
+      fadeOutDuration: 600, 
+      keepRelativePositionOnResize: true,
+  },
+  lines: {
+      minDistance: 0.15,
+      color: "0xFFFFFF",
+      maxOpacity: 0.4,
+      thickness: 1,
+      distanceBasedTransparency: true,
+  },
+    })
+  }
 }
